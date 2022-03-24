1cd26e071f4b693e9dac11dae1dbaa56332e76cb
rizkyr117h <84673678+rizkyr117h@users.noreply.github.com>
2022-03-24 22:08:48 +0700

    menambahkan class SayaTubeVideo

--- tpmodul5_1302203122/tpmodul5_1302203122/Program.cs
+++ tpmodul5_1302203122/tpmodul5_1302203122/Program.cs
@@ -10,6 +10,32 @@ namespace tpmodul5_1302203122
     {
         static void Main(string[] args)
         {
+            SayaTubeVidio a = new SayaTubeVidio("Tutorial Design By Contract – Rizky Ramdhan Nugraha");
+            a.printVideoDetails();
         }
     }
+    class SayaTubeVidio
+    {
+        int id;
+        string title;
+        int playCount;
+
+        public SayaTubeVidio(string judul)
+        {
+            title = judul;
+            Random randm = new Random();
+            id = randm.Next(10000, 99999);
+            playCount = 0;
+        }
+        public void printVideoDetails()
+        {
+            Console.WriteLine("Title : " + title);
+            Console.WriteLine("id : " + id);
+            Console.WriteLine("playCount : " + playCount);
+        }
+
+    }
+
+
+
 }
