563b0ca894786489e88cb9873262c08455d1eded
rizkyr117h <84673678+rizkyr117h@users.noreply.github.com>
2022-03-25 16:52:20 +0700

    implementasi design by contract

--- tpmodul5_1302203122/tpmodul5_1302203122/Program.cs
+++ tpmodul5_1302203122/tpmodul5_1302203122/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,18 +11,22 @@ namespace tpmodul5_1302203122
     {
         static void Main(string[] args)
         {
-            try
-            {
+
                 SayaTubeVidio a = new SayaTubeVidio("Tutorial Design By Contract – Rizky Ramdhan.");
                 Console.WriteLine("Massukan angka : ");
                 int b = Convert.ToInt32(Console.ReadLine());
                 a.increasePlayCount(b);
                 a.printVideoDetails();
-            }
-            catch
-            {
-                Console.WriteLine("ERROR");
-            }
+
+                SayaTubeVidio c = new SayaTubeVidio("Tutorial Design By Contract – Rizky Ramdhan.");
+                
+                for (int i = 0; i < 215; i++)
+                {
+                    c.increasePlayCount(100000000);
+                }
+
+                c.printVideoDetails();
+
         }
         class SayaTubeVidio
         {
@@ -31,13 +36,23 @@ namespace tpmodul5_1302203122
 
             public SayaTubeVidio(string judul)
             {
-                if (judul != null && judul.Length < 100)
+                Debug.Assert(judul != null, "title tidak boleh kosong");
+                Debug.Assert(judul.Length < 100, "title kepanjangan");
+
+                string test;
+
+                try
                 {
+                    test = checked(judul);
                     title = judul;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
                 }
 
                 Random randm = new Random();
-                id = randm.Next(10000, 99999);
+                id = randm.Next(0, 100000);
                 playCount = 0;
             }
             public void printVideoDetails()
@@ -49,9 +64,19 @@ namespace tpmodul5_1302203122
 
             public void increasePlayCount(int data)
             {
-                if (data < 10000001)
+                Debug.Assert(this.playCount <= int.MaxValue);
+                Debug.Assert(data <= 10000000, "Input tidak boleh lebih dari 10 juta");
+
+                int test;
+                try
                 {
-                    playCount = playCount + data;
+                    test = checked(this.playCount + data);
+                    this.playCount += data;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error : " + ex.Message);
+                    this.playCount = this.playCount;
                 }
 
             }
