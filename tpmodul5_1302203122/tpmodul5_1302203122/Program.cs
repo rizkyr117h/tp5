337df126e2f28b91779596862f4fc3b4315a4592
rizkyr117h <84673678+rizkyr117h@users.noreply.github.com>
2022-03-25 16:11:02 +0700

    implementasi design by contract

--- tpmodul5_1302203122/tpmodul5_1302203122/Program.cs
+++ tpmodul5_1302203122/tpmodul5_1302203122/Program.cs
@@ -10,30 +10,52 @@ namespace tpmodul5_1302203122
     {
         static void Main(string[] args)
         {
-            SayaTubeVidio a = new SayaTubeVidio("Tutorial Design By Contract – Rizky Ramdhan Nugraha");
-            a.printVideoDetails();
+            try
+            {
+                SayaTubeVidio a = new SayaTubeVidio("Tutorial Design By Contract – Rizky Ramdhan.");
+                Console.WriteLine("Massukan angka : ");
+                int b = Convert.ToInt32(Console.ReadLine());
+                a.increasePlayCount(b);
+                a.printVideoDetails();
+            }
+            catch
+            {
+                Console.WriteLine("ERROR");
+            }
         }
-    }
-    class SayaTubeVidio
-    {
-        int id;
-        string title;
-        int playCount;
-
-        public SayaTubeVidio(string judul)
+        class SayaTubeVidio
         {
-            title = judul;
-            Random randm = new Random();
-            id = randm.Next(10000, 99999);
-            playCount = 0;
-        }
-        public void printVideoDetails()
-        {
-            Console.WriteLine("Title : " + title);
-            Console.WriteLine("id : " + id);
-            Console.WriteLine("playCount : " + playCount);
-        }
+            int id;
+            string title;
+            int playCount;
 
+            public SayaTubeVidio(string judul)
+            {
+                if (judul != null && judul.Length < 100)
+                {
+                    title = judul;
+                }
+
+                Random randm = new Random();
+                id = randm.Next(10000, 99999);
+                playCount = 0;
+            }
+            public void printVideoDetails()
+            {
+                Console.WriteLine("Title : " + title);
+                Console.WriteLine("id : " + id);
+                Console.WriteLine("playCount : " + playCount);
+            }
+
+            public void increasePlayCount(int data)
+            {
+                if (data < 10000001)
+                {
+                    playCount = playCount + data;
+                }
+
+            }
+        }
     }
 
 
